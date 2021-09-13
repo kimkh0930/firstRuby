81ce6fe03bcad2b3492d05662ffcb42dcc439ffa
김경환 <kimkh940930@gmail.com>
2021-09-13 14:43:34 +0000

    create plus page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
 	def index
+
 	end
 	
-	def hi
-		@show_message = true
-		@message = "bye"
+	def result
+		@plus_result = params[:num1].to_i + params[:num2].to_i
 	end
 end
